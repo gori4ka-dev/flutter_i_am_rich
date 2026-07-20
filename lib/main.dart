f26242c24318f2ddcbd4d0911305b6d4d17503bc
gori4ka-dev <gori4ka.developer@gmail.com>
2026-07-21 01:10:30 +0300

    Stateless > Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,15 @@ void main() {
   runApp(IAmRichApp());
 }
 
-class IAmRichApp extends StatelessWidget {
+class IAmRichApp extends StatefulWidget {
   IAmRichApp({super.key});
+
+  @override
+  State<IAmRichApp> createState() => _IAmRichAppState();
+}
+
+class _IAmRichAppState extends State<IAmRichApp> {
+  int money = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -13,24 +20,67 @@ class IAmRichApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blueGrey[700],
         appBar: AppBar(
-          title: Text('I Am Rich 1'),
+          title: Text('I Am Rich'),
           centerTitle: true,
           backgroundColor: Colors.blueGrey[300],
         ),
         body: Center(
-          child:Column(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset('assets/images/diamond.png',width: 180),
+              Image.asset('assets/images/diamond.png', width: 180),
               SizedBox(height: 30),
               Text(
-                'I Am Rich 2',
+                'I Am Rich',
                 style: TextStyle(
                   fontSize: 40,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
                 ),
-              )
+              ),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        money += 100;
+                      });
+                    },
+                    child: Text('Стать богаче'),
+                  ),
+                  SizedBox(width: 5),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        money = 0;
+                      });
+                    },
+                    child: Text('Потратить всё состояние'),
+                  )
+                ],
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(width: 20),
+                  Text(
+                    'Моё состояние:',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  SizedBox(width: 5),
+                  Text(
+                    '\$$money',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
